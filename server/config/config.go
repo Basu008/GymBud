d9f1e22d33d498e2b3a8f4a95480f1834706c14c
Basu <basu.singh05@gmail.com>
2026-04-19 02:41:22 +0530

    setting up config file

--- server/config/config.go
+++ server/config/config.go
@@ -1,3 +1,65 @@
 package config
 
-type Config struct{}
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerConfig           ServerConfig           `mapstructure:"server"`
+	TokenAuthConfig        TokenAuthConfig        `mapstructure:"tokenAuth"`
+	MongoDatabaseConfig    MongoDatabaseConfig    `mapstructure:"mongoDatabase"`
+	PostgresDatabaseConfig PostgresDatabaseConfig `mapstructure:"postgresDatabase"`
+	RedisConfig            RedisConfig            `mapstructure:"redis"`
+	MiddleWareConfig       MiddleWareConfig       `mapstructure:"middleWare"`
+	AdditionalConfig       AdditionalConfig       `mapstructure:"additional"`
+}
+
+type ServerConfig struct {
+	ListenAddress      string `mapstructure:"listenAddress"`
+	Port               string `mapstructure:"port"`
+	WorkerPort         string `mapstructure:"workerPort"`
+	ReadTimeout        string `mapstructure:"readTimeout"`
+	WriteTimeout       string `mapstructure:"writeTimeout"`
+	Environment        string `mapstructure:"environment"`
+	CORSAllowedOrigins string `mapstructure:"corsAllowedOrigins"`
+}
+
+type TokenAuthConfig struct {
+	JWTSignKey   string `mapstructure:"jwtSignKey"`
+	JWTExpiresAt string `mapstructure:"jwtExpiresAt"`
+}
+
+type MongoDatabaseConfig struct {
+}
+
+type PostgresDatabaseConfig struct {
+}
+
+type RedisConfig struct {
+}
+
+type MiddleWareConfig struct {
+}
+
+type AdditionalConfig struct {
+}
+
+func LoadConfig() Config {
+	var cfg Config
+
+	v := viper.New()
+	v.SetConfigFile("conf/default.toml")
+	v.SetConfigType("toml")
+
+	if err := v.ReadInConfig(); err != nil {
+		log.Fatalf("failed to read config file: %v", err)
+	}
+
+	if err := v.Unmarshal(&cfg); err != nil {
+		log.Fatalf("failed to unmarshal config: %v", err)
+	}
+
+	return cfg
+}
